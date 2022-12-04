d140b43e5f3fd0986646db544c3f8bd1d58f8b68
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-12-05 00:57:52 +0900

    Update use server

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,10 @@ import './App.css';
 import axios from "axios";
 
 function App() {
-  const query = '\n    query questionOfToday {\n  activeDailyCodingChallengeQuestion {\n    date\n    userStatus\n    link\n    question {\n      acRate\n      difficulty\n      freqBar\n      frontendQuestionId: questionFrontendId\n      isFavor\n      paidOnly: isPaidOnly\n      status\n      title\n      titleSlug\n      hasVideoSolution\n      hasSolution\n      topicTags {\n        name\n        id\n        slug\n      }\n    }\n  }\n}\n    ';
-
   useEffect(() => {
-    axios.post('/graphql', {query: query}).then((response) => {
-      const link = response.data.data.activeDailyCodingChallengeQuestion.link;
-
-      console.log(link);
-
-      window.close();
-      window.open('https://leetcode.com' + link, '_self');
+    axios.get<string>('https://lctp.fly.dev/').then((response) => {
+      console.log(response)
+      window.open(response.data, '_self');
     });
   }, []);
 
