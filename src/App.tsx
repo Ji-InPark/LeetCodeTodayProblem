27e903ee0bfac291313f99c19a4fa3c1f1258b33
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-12-18 22:22:24 +0900

    Apply ga4

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import axios from "axios";
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-K9ZBLSZZFE');
 
 function App() {
+  ReactGA.send({hitType: 'pageview', page: '/'});
+  
   useEffect(() => {
     axios.get<string>('https://lctp.fly.dev/').then((response) => {
       console.log(response)
